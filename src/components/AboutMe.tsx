b813e8f70362cb6d6f593f1336ec4fd477f086cf
shinwonkim <shinwonkkim@gmail.com>
2025-05-09 18:42:21 +0900

    update: 내용 추가

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,6 +10,7 @@ gsap.registerPlugin(ScrollTrigger, useGSAP);
 const certificate = [
   { name: "프론트엔드 교육 과정", detail: "제로베이스 부트캠프 (2025.03 수료)" },
   { name: "정보처리기사", detail: "한국산업인력공단 (2024.12 취득)" },
+  { name: "Toeic Speaking AL", detail: "ETS (2024.03 취득)" },
   { name: "Delf A2", detail: "France Education International (2020.01 취득)" },
 ];
 
