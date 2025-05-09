5ccf00149cab158d15d01adffb15c1d19b9dac26
shinwonkim <shinwonkkim@gmail.com>
2025-05-09 11:33:30 +0900

    style: 전체 화면 시 크기 수정, hover 색상 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,5 @@
 import { JSX, useState } from "react";
 import { Link } from "react-scroll";
-
 const Nav = (): JSX.Element => {
     const [activeLink, setActiveLink] = useState<string>("home");
 
@@ -9,22 +8,22 @@ const Nav = (): JSX.Element => {
     };
 
     return (
-        <nav className="fixed flex justify-end w-full max-w-[1200px] gap-4 p-4 text-sm font-bold cursor-pointer py-2 z-50 bg-whiteColor/80 backdrop-blur">
+        <nav className="fixed flex justify-end w-full gap-4 p-4 xl:pr-32 py-2 text-sm font-bold cursor-pointer z-50 text-navyColor bg-whiteColor/80 backdrop-blur">
             {activeLink !== "home" && (
                 <Link to="home" smooth={true} duration={500} spy={true} onSetActive={() => setActiveLink("home")} 
-                    className={`hover:bg-grayColor/60 px-1 rounded-sm ${activeLink==="home" ? "text-blueColor": ""}`} onClick={()=>handleClick("home")}> Home
+                    className={`hover:bg-grayColor/60 hover:text-blueColor px-1 rounded-sm ${activeLink==="home" ? "text-blueColor": ""}`} onClick={()=>handleClick("home")}> Home
                 </Link>
             )}
             <Link to="aboutMe" smooth={true} duration={500} spy={true} onSetActive={() => setActiveLink("aboutMe")} 
-                className={`hover:bg-grayColor/60 px-1 rounded-sm ${activeLink==="aboutMe"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
+                className={`hover:bg-grayColor/60 hover:text-blueColor px-1 rounded-sm ${activeLink==="aboutMe"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
                 onClick={()=>handleClick("aboutMe")} > About me
             </Link>
             <Link to="projects" smooth={true} duration={500} spy={true} onSetActive={() => setActiveLink("projects")} 
-                className={`hover:bg-grayColor/60 px-1 rounded-sm  ${activeLink==="projects"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
+                className={`hover:bg-grayColor/60 hover:text-blueColor px-1 rounded-sm  ${activeLink==="projects"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
                 onClick={()=>handleClick("projects")}> Projects
             </Link>
             <Link to="career" smooth={true} duration={500} spy={true} onSetActive={() => setActiveLink("career")}
-                className={ `hover:bg-grayColor/60 px-1 rounded-sm ${activeLink==="career"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
+                className={ `hover:bg-grayColor/60 hover:text-blueColor px-1 rounded-sm ${activeLink==="career"  ? "text-blueColor underline underline-offset-5 decoration-2": ""}`} 
                 onClick={()=>handleClick("career")}> Career
             </Link>
             {/* <Link to="contact" smooth={true} duration={500} spy={true} onSetActive={() => setActiveLink("contact")}
@@ -36,8 +35,3 @@ const Nav = (): JSX.Element => {
 }
 
 export default Nav;
-
-// import clsx from "clsx";
-// className={clsx({
-//     "text-blueColor underline underline-offset-5 decoration-2": activeLink === "aboutMe",
-//   })}
