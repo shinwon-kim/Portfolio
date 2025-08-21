fee753da2d22861d2d4fc3eb166380912a6a5769
shinwonkim <shinwonkkim@gmail.com>
2025-08-21 22:55:05 +0900

    feat: Contact 컴포넌트 이메일 다국어 기능 구현

--- src/common/ContactBadge.tsx
+++ src/common/ContactBadge.tsx
@@ -8,7 +8,8 @@ const ContactBadge=(): JSX.Element =>{
     const [copied, setCopied] = useState(false);
     const email = "shinwonkkim@gmail.com";
     const githubUrl  = "https://github.com/shinwon-kim";
-    
+    const userLang = navigator.language.startsWith("ko") ? "ko" : "en";
+
     const handleCopy = () => {
         navigator.clipboard.writeText(email).then(() => {
             setCopied(true);
@@ -21,31 +22,28 @@ const ContactBadge=(): JSX.Element =>{
             {/* 이메일 아이콘 */}
             <div 
                 className="relative"
-                // className="w-10 h-10 bg-grayColor/78 rounded-full flex justify-center items-center"
                 onMouseEnter={() => setShowEmailTooltip(true)}
                 onMouseLeave={() =>{
                     setShowEmailTooltip(false);
                     setCopied(false);
                 }}
             >
-                    {showEmailTooltip && (
-                        <div className="absolute top-1/2 -translate-y-1/2 right-12 lg:right-15 bg-black/75 text-white text-xs px-2 py-1 rounded shadow-lg mb-1 whitespace-nowrap">
-                            {copied ? "이메일이 복사되었습니다" : email}
-                        </div>
-                    )}
-                    <button 
-                        onClick={handleCopy}
-                        className="w-10 h-10 sm:w-11 sm:h-11 lg:w-13 lg:h-13 bg-grayColor/80 rounded-full flex justify-center items-center hover:animate-pulse transition"
-                        // title="이메일 복사"
-                    >
-                        <MdOutlineEmail className="text-2xl fill-navyColor"></MdOutlineEmail>
-                    </button>    
+                {showEmailTooltip && (
+                    <div className="absolute top-1/2 -translate-y-1/2 right-12 lg:right-15 bg-black/75 text-white text-xs px-2 py-1 rounded shadow-lg mb-1 whitespace-nowrap">
+                        {copied ? userLang === "ko" ? "이메일이 복사되었습니다": "Email copied!" : email}
+                    </div>
+                )}
+                <button 
+                    onClick={handleCopy}
+                    className="w-10 h-10 sm:w-11 sm:h-11 lg:w-13 lg:h-13 bg-grayColor/80 rounded-full flex justify-center items-center hover:animate-pulse transition"
+                >
+                    <MdOutlineEmail className="text-2xl fill-navyColor"></MdOutlineEmail>
+                </button>    
 
             </div>
 
             {/* 깃허브 아이콘 */}
             <div 
-                // className="w-10 h-10 bg-grayColor/78 rounded-full flex justify-center items-center"
                 className="relative"
                 onMouseEnter={() => setShowGithubTooltip(true)}
                 onMouseLeave={() =>{
