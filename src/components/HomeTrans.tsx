de331e1189329aa5724a8d26edeb31a6e2c81a32
shinwonkim <shinwonkkim@gmail.com>
2025-10-18 00:35:47 +0900

    docs: 문구 수정

--- src/components/HomeTrans.tsx
+++ src/components/HomeTrans.tsx
@@ -1,13 +1,13 @@
 export const homeTrans = {
     ko: {
-        role: "프론트엔드 개발자",
+        // role: "프론트엔드 개발자",
         name: ["김", "신", "원"],
         nameSuffix: "입니다",
         // intro: <>무언가를 만드는 것이 좋아 프로그래밍을 시작했으며, 사용자가 직접 마주하는 화면을 설계하고 구현하는 과정에 흥미를 느끼게 되었습니다. <br/>직관적인 디자인과 뛰어난 사용자 경험을 고민하며, 최고의 UI/UX를 실현하는 프론트엔드 개발자가 목표입니다.</>,
         intro: <>무언가를 만드는 것을 좋아해 프로그래밍을 시작했으며, 기술을 통해 사람들의 경험을 더 편리하게 만드는 일에 흥미를 느끼게 되었습니다.<br/>직관적인 디자인과 효율적인 기능 구현을 고민하며, 사용자 중심의 솔루션을 만들어가는 것을 목표로 하고 있습니다.</>,
     },
     en: {
-        role: "Frontend Developer",
+        // role: "Frontend Developer",
         name: ["KIM","SHIN", "WON"],
         nameSuffix: "",
         // intro: <>I began programming out of a passion for creating, and soon discovered my interest in designing and building interfaces that users directly interact with. <br/>With a focus on intuitive design and seamless user experiences, I aspire to grow as a frontend developer who delivers outstanding UI and UX.</>
